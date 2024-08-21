e0f913ae4ccb291c3fd1cfef59c0b33910e1710e
Omar Ayoub <omarnmayoub@gmail.com>
2024-08-21 04:14:52 +0300

    Project Files

---
+++ TivaUART/Objects/main.d
@@ -0,0 +1,5 @@
+./objects/main.o: main.c \
+  C:\Users\Omar\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
